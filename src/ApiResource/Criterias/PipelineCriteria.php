6fbd7f69ffcf340aabb28f071a5876e5fd04f902
Allen Jay Desamparado <ajDesamparado13@gmail.com>
2020-10-05 17:13:50 +0800

    UPDATES

--- src/ApiResource/Criterias/PipelineCriteria.php
+++ src/ApiResource/Criterias/PipelineCriteria.php
@@ -14,6 +14,7 @@ abstract class PipelineCriteria extends BaseResourceCriteria
 {
     abstract public function getCriterias() : array;
 
+
     public function getFields() : array {
         return ['*'];
     }
@@ -24,18 +25,37 @@ abstract class PipelineCriteria extends BaseResourceCriteria
 
     public function handle($model){
         $inputs = $this->makeInputs();
-        foreach($this->getCriterias() as $criteria){
-            if(is_string($criteria)){
-                $criteria = app()->makeWith($criteria,[ 'inputs' => $inputs ]);
-            }
-
-            if(!$criteria instanceof BaseResourceCriteria){
-                throw new ResourceCriteriaException("Class " . get_class($criteria) . " must be an instance of" . BaseResourceCriteria::class );
-            }
-
-            $model = $criteria->handle($model);
+        if($this->shouldSkipCriteria($inputs)){
+            return $model;
         }
-        return $model;
+        return $this->newQuery($model,$inputs);
+    }
+
+    protected function newQuery($model,array $inputs){
+        return $this->buildQuery($model,$this->getCriterias(),$inputs);
+    }
+
+    protected function buildQuery($query,array $criterias,array $inputs){
+        foreach($criterias as $criteria){
+            $criteria = $this->makeCriteria($criteria,$inputs);
+            $query = $this->specialQuery($query,$criteria,$inputs);
+        }
+        return $query;
+    }
+
+    protected function makeCriteria($criteria,array $inputs){
+        if(is_string($criteria)){
+            $criteria = app()->makeWith($criteria,[ 'inputs' => $inputs ]);
+        }
+
+        if(!$criteria instanceof BaseResourceCriteria){
+            throw new ResourceCriteriaException("Class " . get_class($criteria) . " must be an instance of" . BaseResourceCriteria::class );
+        }
+        return $criteria;
+    }
+
+    protected function specialQuery($query,$criteria,array $inputs){
+        return $criteria->handle($query);
     }
 
 }
