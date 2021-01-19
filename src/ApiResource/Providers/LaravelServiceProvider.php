653db4a41508e22cb4d11d7a494785590741c8f6
Allen Jay Desamparado <ajDesamparado13@gmail.com>
2021-01-19 17:23:14 +0800

    Fix attaching of additional response on resource macro

--- src/ApiResource/Providers/LaravelServiceProvider.php
+++ src/ApiResource/Providers/LaravelServiceProvider.php
@@ -41,18 +41,18 @@ class LaravelServiceProvider extends ServiceProvider
     protected function loadResponseMacro()
     {
         Response::macro('resource',function($data,JsonResourceInterface $transformer, array $response=[]){
-            $transformer->resource = $data;
             $is_resource_collection = is_a($data,'Illuminate\Pagination\LengthAwarePaginator') || is_a($data,'Illuminate\Database\Eloquent\Collection');
             $is_resource_model = is_a($data,'Illuminate\Database\Eloquent\Model');
-            if($is_resource_collection || $is_resource_model){
-                $transformer->additional($response);
-                $result = call_user_func_array(array($transformer,$is_resource_collection ? 'collection' : 'make'),[ $data ]);
-                $statusCode = empty($result->response()->getData()->data) ? 204 : 200;
-                $result->response()->setStatusCode($statusCode);
-                return $result;
+            if(!( $is_resource_collection || $is_resource_model )){
+                $response['data'] = $data;
+                return Response::json($response,empty($data) ? 200 : 204);
             }
-            $response['data'] = $data;
-            return Response::json($response,empty($data) ? 200 : 204);
+
+            $result = call_user_func_array(array($transformer,$is_resource_collection ? 'collection' : 'make'),[ $data ]);
+            $statusCode = empty($result->response()->getData()->data) ? 204 : 200;
+            $result->response()->setStatusCode($statusCode);
+            $result->additional($response);
+            return $result;
         });
 
         Response::macro('unauthorized', function ($message="Not Authenticated") {
